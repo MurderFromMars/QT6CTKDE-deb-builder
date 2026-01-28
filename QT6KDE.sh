5220b5073c918f4c4dee938346888b5f9b6677af
MurderFromMars <untamedprodigy67@gmail.com>
2026-01-28 11:19:07 -0500

    Update QT6KDE.sh

--- QT6KDE.sh
+++ QT6KDE.sh
@@ -78,7 +78,7 @@ print_header "${ICON_ROCKET} Building qt6ct-kde v${VERSION}"
 
 # Dependencies
 step "${ICON_GEAR} Checking dependencies"
-DEPS=("build-essential" "cmake" "ninja-build" "qt6-base-dev" "qt6-base-private-dev" "qt6-tools-dev" "libqt6svg6-dev" "qml6-module-qtquick-controls" "git")
+DEPS=("build-essential" "cmake" "ninja-build" "qt6-base-dev" "qt6-base-private-dev" "qt6-tools-dev" "libqt6svg6-dev" "qml6-module-qtquick-controls" "libkf6qqc2desktopstyle-dev" "git")
 MISSING=()
 
 for dep in "${DEPS[@]}"; do
@@ -266,7 +266,7 @@ Priority: optional
 Architecture: ${ARCH}
 Installed-Size: ${INSTALLED_SIZE}
 Maintainer: qt6ct-kde Builder <noreply@example.com>
-Depends: libqt6core6, libqt6gui6, libqt6widgets6, libqt6svg6, qml-module-qtquick-controls, qml-module-org-kde-qqc2breezestyle
+Depends: libqt6core6, libqt6gui6, libqt6widgets6, libqt6svg6, qml-module-qtquick-controls, qml-module-org-kde-qqc2desktopstyle
 Description: Qt6 Configuration Utility patched for KDE
  qt6ct is a program that allows users to configure Qt6 settings
  (theme, font, icons, etc.) under desktop environments other than KDE.
